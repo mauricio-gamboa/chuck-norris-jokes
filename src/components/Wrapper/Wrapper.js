4f74d0c803fd815d189b67910bba494c9d47905b
Mauricio Gamboa <Mauricio@fscmfps82g2.sephoraus.com>
2019-06-17 18:39:02 -0600

    Create Wrapper component.

---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Wrapper() {
+    return (
+        <div>Hallo, Chuck Norris-fans!</div>
+    );
+}
+
+export default Wrapper;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Wrapper from './components/Wrapper/Wrapper';
+
+ReactDOM.render(<Wrapper />, document.getElementById('root'));
