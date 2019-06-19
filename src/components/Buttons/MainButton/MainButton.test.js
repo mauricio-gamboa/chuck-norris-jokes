051bf4a6b22e10fb59a0e3e6218ee963ac45808b
Mauricio Gamboa <Mauricio@mau.lan>
2019-06-18 22:48:20 -0600

    Added tests to the MainButton component.

--- src/components/Buttons/MainButton/MainButton.test.js
+++ src/components/Buttons/MainButton/MainButton.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import MainButton from './MainButton';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<MainButton />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('MainButton', () => {
+    let component;
+    let handleClickMock;
+    const text = 'test text';
+
+    beforeEach(() => {
+        handleClickMock = jest.fn();
+        component = shallow(<MainButton handleClick={handleClickMock}>{text}</MainButton>);
+    });
+
+    test('calls the handleClick prop on click', () => {
+        component.find('button').simulate('click');
+        expect(handleClickMock).toHaveBeenCalledTimes(1);
+    });
+
+    test('contains the correct text', () => {
+        expect(component.text()).toEqual(text);
+    });
 });
